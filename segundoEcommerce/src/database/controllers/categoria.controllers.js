b854d1cd38649f7e0c61c8ab4dede769117d85cb
Yolaus Godoy <yolausgodoy07@gmail.com>
2023-11-11 16:33:33 -0300

    Crud

--- segundoEcommerce/src/database/controllers/categoria.controllers.js
+++ segundoEcommerce/src/database/controllers/categoria.controllers.js
@@ -2,37 +2,68 @@ import { pool } from "../db.js"
 
 export const getCategorias = async (req, res) => {
 
-    await pool.promise().query("Select * from Categoria ")
-        .then(([rows]) => {
-            console.log(rows);
-
-            res.json({
-                rows
-            })
+    const [result] = await pool.promise().query("Select * from Categoria ")
+    try {
+        res.json({
+            result
         })
-        .catch(console.log)
+    } catch (error) {
+        console.log(error)
+    }
 }
-export const getCategoria = (req, res) => {
-    res.send('Obteniendo Categoria')
-}
+export const getCategoria = async (req, res) => {
+
+    const [result] = await pool.promise().query("Select * from Categoria where idCategoria = ? ", [req.params.id]);
+    try {
+        if (result === 0)
+            return res.status(404).json({ message: "No existe la categoria" })
+
+        res.json(
+            result[0]
+        )
+
+    } catch (error) {
+        console.log(error)
+    }
+};
 export const createCategoria = async (req, res) => {
 
     console.log(req.body)
     const { nombre } = req.body;
-    await pool.promise().query("insert into Categoria (nombre) values(?)", [nombre])
-        .then(([rows]) => {
-            console.log(rows);
-            res.json({
-                id: rows.insertId,
-                nombre
-            })
-            res.send('Creando Categoria')
+    const [result] = await pool.promise().query("insert into Categoria (nombre) values(?)", [nombre])
+    try {
+        console.log(result);
+        res.json({
+            id: result.insertId,
+            nombre
         })
-        .catch(console.log)
+    } catch (error) {
+        console.log(error)
+    }
+
+
 }
-export const updateCategoria = (req, res) => {
-    res.send('Actualizando  Categoria')
+export const updateCategoria = async (req, res) => {
+    const { nombre } = req.body
+    const [result] = await pool.promise().query("Update Categoria Set nombre = ? Where idCategoria = ?", [nombre, req.params.id])
+    try {
+        res.json({
+            result
+        })
+    } catch (error) {
+        console.log(error)
+    }
+
 }
-export const deleteCategoria = (req, res) => {
-    res.send('Eliminando  Categoria')
+export const deleteCategoria = async (req, res) => {
+    const [result] = await pool.promise().query("Delete from Categoria where idCategoria = ? ", [req.params.id]);
+    try {
+        if (result === 0)
+            return res.status(404).json({ message: "No existe la categoria" })
+
+        return res.status(204)
+
+    } catch (error) {
+        console.log(error)
+    }
 }
